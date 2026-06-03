345594467237f2e8175c11bcb67046dae018fd54
sinsia27 <syysia2787@gmail.com>
2026-06-04 08:56:23 +0900

    fix: Main 클래스 누락 추가 (JipsaServerApplication)

---
+++ src/main/java/com/jipsa/jipsaserver/JipsaServerApplication.java
@@ -0,0 +1,13 @@
+package com.jipsa.jipsaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JipsaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JipsaServerApplication.class, args);
+    }
+
+}
